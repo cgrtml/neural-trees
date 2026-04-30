9eb0e917192d8001de549f4c2b4a1b9ee4d9a1b1
Cagri Temel <cagritemel34@gmail.com>
2026-04-29 20:22:45 -0700

    Bump version to 0.1.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 setup(
     name="neural-trees",
-    version="0.1.0",
+    version="0.1.1",
     author="Cagri Temel",
     author_email="cagritemel34@gmail.com",
     description=(
